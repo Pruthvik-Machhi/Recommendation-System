cc0b76f8829bef6561bbf52ef1d5b96c02930f13
Pruthvik-Machhi <pruthvikmachhi7@gmail.com>
2024-01-26 14:17:04 +0530

    laoding dataset

--- scrip.py
+++ scrip.py
@@ -1,2 +1,4 @@
 import numpy as np
 import pandas as pd
+movies = pd.read_csv("C:\\Users\\pruth\\Downloads\\tmdb_5000_movies.csv.zip")
+credits = pd.read_csv("C:\\Users\\pruth\\Downloads\\tmdb_5000_credits.csv.zip") 
