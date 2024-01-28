3f77e9d0a10ae4335544300bf32df6e70bc5e3b4
Pruthvik-Machhi <pruthvikmachhi7@gmail.com>
2024-01-28 15:25:50 +0530

    merging dataset using title

--- scrip.py
+++ scrip.py
@@ -2,3 +2,7 @@ import numpy as np
 import pandas as pd
 movies = pd.read_csv("C:\\Users\\pruth\\Downloads\\tmdb_5000_movies.csv.zip")
 credits = pd.read_csv("C:\\Users\\pruth\\Downloads\\tmdb_5000_credits.csv.zip") 
+movies = movies.merge(credits,on='title')
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+movies.head()
+print(movies)
