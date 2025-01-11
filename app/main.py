4e652e1dbb0e85cdc8e1c475befe68b616d2b187
Pruthvik <156055812+Pruthvik-Machhi@users.noreply.github.com>
2025-01-11 12:03:22 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 # from app.data import movies, books
 # import pandas as pd
-from app.models import recommend_movies, recommend_books, predict_sentiment
+from models import recommend_movies, recommend_books, predict_sentiment
 
-from app.streamlit_components import movies_ui, books_ui
+from streamlit_components import movies_ui, books_ui
 
 def main():
     st.title("Recommendation and Sentiment Analysis System")
